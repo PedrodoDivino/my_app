77ddd1a443733f0de7fc9805b1ff7c19a4670b72
Pedro Henrique <Pedrohenriquebatera@hotmail.com>
2021-12-06 13:09:47 -0300

    estudos em react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import HelloWord from './hello.js';
+import Lorem from './components/lorem';
+
+
+
+
+function App() {
+  return (
+    <div>
+<HelloWord/>
+<Lorem/>
+    </div>
+
+    
+  );
+}
+export default App;
+
