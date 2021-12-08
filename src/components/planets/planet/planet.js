74a4663fcd3be3468cdf5c597c060b3fb358b101
Pedro Henrique <Pedrohenriquebatera@hotmail.com>
2021-12-08 15:33:01 -0300

    finalizando dia 08

--- src/components/planets/planet/planet.js
+++ src/components/planets/planet/planet.js
@@ -1,4 +1,4 @@
-import React , { fragment }from 'react';
+import React, { fragment } from 'react';
 import GrayImg from '../../shared/gray_img/grayImg.js';
 import DescriptionWhitLink from '../../shared/DescriptionWhitLink/DescriptionWithLink.js';
 
@@ -7,10 +7,10 @@ const Planet = (props) => {
     return (
         <div>
             <h4>{props.name}</h4>
-            < DescriptionWhitLink description={props.description} link={props.link}/>
-          <GrayImg img_url={props.img_url}/> 
-          
-     
+            < DescriptionWhitLink description={props.description} link={props.link} />
+            <GrayImg img_url={props.img_url} />
+
+
         </div>
     )
 }
